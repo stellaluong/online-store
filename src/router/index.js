23ae1a2d2776b4c34cae96e27f73d7845489dd20
stellaluong <57662112+stellaluong@users.noreply.github.com>
2020-06-08 14:52:50 +0200

    x

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,27 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import Product from './views/Product.vue';
 
-Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
-];
+Vue.use(Router);
 
-const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/products/:id',
+      name: 'product',
+      component: Product,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+    },
+  ],
 });
-
-export default router;
--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,62 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-});
+    state: {
+        cart: [],
+        products: [
+            {
+                name: "Base Organic cotton t-shirt",
+                id: '001',
+                price: 100,
+                color: "Hvit",
+                size: "Small",
+                gender: "Dame",
+                quantity: 10,
+                dateAdded: "",
+                category: "Topper",
+                details: {
+                    material: "Organic Cotton",
+                    fit: "Normal passform",
+                    maintancence: "Maskin vask",
+                    additional: "",
+                },
+                images: [
+                    'white-01-1.jpg',
+                    'white-01-2.jpg',
+                    'white-01-3.jpg',
+                    'white-01-4.jpg',
+                ]
+            },
+            {
+                name: "Arch Light Jeans",
+                id: '002',
+                price: 599,
+                color: "Blue",
+                size: "Medium",
+                gender: "Dame",
+                quantity: 8,
+                dateAdded: "",
+                category: "Jeans",
+                details: {
+                    material: "Bomull",
+                    fit: "High Waist",
+                    maintancence: "Maskin vask",
+                    additional: "",
+                },
+                images: [
+                    'jeans-01-1.jpg',
+                    'jeans-01-2.jpg',
+                    'jeans-01-3.jpg',
+                    'jeans-01-4.jpg',
+                    ]
+            }
+        ]
+    },
+    mutations: {},
+    actions: {},
+    getters: {
+      product: (state) => (id) => {
+        return state.products.filter(p => p.id === Number(id))[0]
+      }
+    }
+  });
