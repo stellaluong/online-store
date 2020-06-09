c312767b0cabb2dfc93d8d591f7e2050aec5b7bd
stellaluong <57662112+stellaluong@users.noreply.github.com>
2020-06-09 14:57:10 +0200

    x

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -2,20 +2,20 @@
   <div>
     <section class="wrapper">
       <div class="flex-col">
-        <img class="flex-col--2" :src="imagePath(product)" alt="">
+        <img  class="flex-col--2" :src="makeImagePath(product)" alt="">
         <div class="flex-col--2" >
-        <h2>{{ product.name }}</h2>
-        <p>Pris: {{ product.price}},- </p>
-        <p>Størrelse: {{ product.size }} </p>
-        <p>Farge: {{ product.color}} </p>
-        <p><em>{{product.quantity}} Igjen på lager</em></p>
-        <h3>Productinformasjon</h3>
-        <ul>
-          <li>Material: {{product.details.material}}</li>
-          <li>Fit: {{product.details.fit}}</li>
-          <li>Maintenance: {{product.details.maintenance}}</li>
-          <li v-if="product.details.additional">Additional: {{product.details.additional}}</li>
-        </ul>
+          <h2>{{ product.name }}</h2>
+          <p>Price: ${{ product.price }}</p>
+          <p>Size: {{ product.size }}</p>
+          <p>Color: {{ product.color }}</p>
+          <p><em>{{ product.quantity }} left in stock</em></p>
+          <h3>Details</h3>
+          <ul>
+            <li>Material: {{ product.details.material }}</li>
+            <li>Fit: {{ product.details.fit }}</li>
+            <li>Maintenance: {{ product.details.maintenance }}</li>
+            <li v-if="product.details.additional">Additional: {{ product.details.additional }}</li>
+          </ul>
         </div>
       </div>
     </section>
@@ -23,20 +23,19 @@
 </template>
 
 <script>
+import { imagePath } from '@/mixins/imagePath.js'
 export default {
   name: 'product',
+  mixins: [imagePath],
   data() {
     return {
       product: this.$store.getters.product(this.$route.params.id)
     }
-  },
-  methods: {
-    imagePath(product) {
-      return require(`../assets/img/products/${product.images[0]}`)
-    }
   }
 };
 </script>
+
+<style lang="scss">
 .flex-col {
   display: flex;
   align-items: flex-start;
@@ -44,5 +43,4 @@ export default {
 .flex-col--2 {
   width: 50%;
 }
-<style lang="scss">
 </style>
